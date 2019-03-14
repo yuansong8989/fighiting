df95800161a3548fb01c5679145fd511da1d434a
yuansong8989 <2506868324@qq.com>
2019-03-14 09:06:46 +0800

    修改后

---
+++ src/main/java/com/springys/entity/PageParameter.java
@@ -0,0 +1,12 @@
+package com.springys.entity;
+
+import lombok.Data;
+
+/**
+ * Created by yzd on 2019/3/13.
+ */
+@Data
+public class PageParameter {
+    private int pageNum;
+    private  int pagesize;
+}
